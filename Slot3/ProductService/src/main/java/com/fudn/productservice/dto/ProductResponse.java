86adb9520815c3569f9c334fad0d58ad55506c1c
haidang2312 <danghai23122005@gmail.com>
2026-09-16 20:23:55 +0700

    feat(dto): implement ProductResponse record

--- Slot3/ProductService/src/main/java/com/fudn/productservice/dto/ProductResponse.java
+++ Slot3/ProductService/src/main/java/com/fudn/productservice/dto/ProductResponse.java
@@ -1,4 +1,11 @@
 package com.fudn.productservice.dto;
 
-public class ProductResponse {
+import java.math.BigDecimal;
+
+public record ProductResponse(
+        String id,
+        String name,
+        String description,
+        BigDecimal price
+) {
 }
