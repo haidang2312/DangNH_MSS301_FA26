e7a38abb6ebff06b2f2b30687cb76e8d3d1aead7
haidang2312 <danghai23122005@gmail.com>
2026-09-16 20:27:30 +0700

    feat(controller): implement ProductController with REST endpoints

---
+++ Slot3/ProductService/src/main/java/com/fudn/productservice/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.fudn.productservice.controller;
+
+import com.fudn.productservice.dto.ProductRequest;
+import com.fudn.productservice.dto.ProductResponse;
+import com.fudn.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
